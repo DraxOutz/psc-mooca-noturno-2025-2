0b71dab2784eda5fd462363609df26dc0e9668d4
Ryan Rocha <kayquesacanaje@gmail.com>
2025-09-02 21:29:32 -0300

    For.java

--- For.java
+++ For.java
@@ -1,7 +1,7 @@
 public class For {
     
     public static void main(String[] args) {
-        for (int i =1; i <= 20; i++) {
+        for (int i =1; i <= 20; i+=2) { // Inicialização = 1, Numero a ser batido = 20, inclementa 2, pode ser usado ++ para 1.
         System.out.println(i);
         try {
             Thread.sleep(1000); // pausa por 2000 milissegundos (2 segundos)
@@ -11,3 +11,4 @@ public class For {
         }
     }
 }
+
